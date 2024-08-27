4c1dd7309e605b69922a9aea0f6ab565c6715bf9
Morghot2 <maximilian.jaron@gmail.com>
2024-08-27 03:41:36 +0200

    FTY-000000: initial setup

---
+++ api/src/index.ts
@@ -0,0 +1,9 @@
+import { app } from './app';
+
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Craftsy on port ${port}');
+});
---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+const App = () => {
+  console.log("asdasd")
+  const a =2
+
+
+
+
+
+
+
+
+
+
+
+  return <h1>Craftsy</h1>
+};
+export default App;
