82ccf21f3b5846b11d33df4a29e55de2cbb09aed
Morghot2 <maximilian.jaron@gmail.com>
2024-09-13 19:06:32 +0200

    FTY-000006: logged user logic

---
+++ api/src/middleware/index.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import { SESSION_TOKEN } from '../constants';
+import { getUserBySessionToken } from '../db/users';
+
+export const isAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const sessionToken = req.cookies[SESSION_TOKEN];
+
+    if (!sessionToken) {
+      return res.sendStatus(403);
+    }
+
+    const result = await getUserBySessionToken(sessionToken);
+
+    if (!result || result.length === 0) {
+      return res.sendStatus(403);
+    }
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
--- api/src/router/index.ts
+++ api/src/router/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { register, login } from '@/controllers/authentication';
+import { register, login } from '../controllers/authentication';
+import { getAllUsers } from '../controllers/users';
+import { isAuthenticated } from '../middleware';
 
 const router = Router();
 
 router.post('/auth/register', register);
 router.post('/auth/login', login);
+router.get('/users', isAuthenticated, getAllUsers);
 export default router;
